9cb916d44d9bc3c086b5a5b0722183e7f82f1efa
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-09 08:53:21 +1000

    Initial Commit

---
+++ ImageDemoLib/ImageDemoLib.h
@@ -0,0 +1,19 @@
+//
+//  ImageDemoLib.h
+//  ImageDemoLib
+//
+//  Created by Evgenii on 9/06/2015.
+//  Copyright (c) 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageDemoLib.
+FOUNDATION_EXPORT double ImageDemoLibVersionNumber;
+
+//! Project version string for ImageDemoLib.
+FOUNDATION_EXPORT const unsigned char ImageDemoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageDemoLib/PublicHeader.h>
+
+
